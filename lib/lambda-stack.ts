46939ffb9001e6fae45c4e0d032957d7bd24a478
Leo <liyung.chen@tum.de>
2024-10-06 19:46:12 +0200

    add iam role and add function url

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from "aws-cdk-lib";
-import { Code } from "aws-cdk-lib/aws-lambda";
+import { Code, FunctionUrlAuthType } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 import { Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import path from "path";
+import { Effect, PolicyStatement, Role } from "aws-cdk-lib/aws-iam";
 
 interface LambdaStackProps extends cdk.StackProps {
     stageName: string;
@@ -13,10 +14,27 @@ export class LambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props);
 
-        new Function(this, "TaiGerPortalTranscriptAnalyzerLambdaFunction", {
+        const lambdaFunction = new Function(this, "TaiGerPortalTranscriptAnalyzerLambdaFunction", {
             runtime: Runtime.PYTHON_3_9,
             code: Code.fromAsset(path.join(__dirname, "..", "lambda")), // Use the zip artifact from CodeBuild
             handler: "lambda_function.lambda_handler"
         });
+
+        lambdaFunction.addFunctionUrl({
+            authType: FunctionUrlAuthType.AWS_IAM
+        });
+
+        // Reference the existing EC2 instance role (replace 'your-ec2-role-name' with the actual role name)
+        const ec2InstanceRole = Role.fromRoleName(this, "EC2InstanceRole", "ec2_taiger_test_infra");
+
+        // Add an IAM policy that allows the EC2 role to invoke the Lambda function URL
+        lambdaFunction.addToRolePolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["lambda:InvokeFunctionUrl"],
+                resources: [lambdaFunction.functionArn],
+                principals: [ec2InstanceRole]
+            })
+        );
     }
 }
