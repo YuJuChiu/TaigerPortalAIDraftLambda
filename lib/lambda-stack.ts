c6a26ce32c2c1b19a5f820b79c14b4d19428345d
Leo <liyung.chen@tum.de>
2024-10-06 23:08:16 +0200

    add assumeRole approach

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -4,7 +4,15 @@ import { Construct } from "constructs";
 
 import { Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import path from "path";
-import { Effect, PolicyStatement, Role } from "aws-cdk-lib/aws-iam";
+import {
+    AccountRootPrincipal,
+    ArnPrincipal,
+    CompositePrincipal,
+    Effect,
+    PolicyStatement,
+    Role,
+    ServicePrincipal
+} from "aws-cdk-lib/aws-iam";
 import {
     AuthorizationType,
     LambdaIntegration,
@@ -26,13 +34,9 @@ export class LambdaStack extends cdk.Stack {
             handler: "lambda_function.lambda_handler"
         });
 
-        lambdaFunction.addFunctionUrl({
-            authType: FunctionUrlAuthType.AWS_IAM
-        });
-
         // Create API Gateway
-        const api = new RestApi(this, "MyApi", {
-            restApiName: "MyService",
+        const api = new RestApi(this, "TranscriptAnalyzerAPIG", {
+            restApiName: "TranscriptAnalyzer",
             description: "This service handles requests with Lambda."
         });
 
@@ -49,5 +53,35 @@ export class LambdaStack extends cdk.Stack {
         // Create a resource and method in API Gateway
         const lambdaResource = api.root.addResource("analyze");
         lambdaResource.addMethod("GET", lambdaIntegration, methodOptions);
+
+        // Create an IAM role for the authorized client
+        const clientRole = new Role(this, "AuthorizedClientRole", {
+            assumedBy: new CompositePrincipal(
+                new ServicePrincipal("ec2.amazonaws.com"),
+                new ArnPrincipal("arn:aws:iam::${AWS_ACCOUNT}:role/ec2_taiger_test_infra")
+                // new ArnPrincipal("arn:aws:iam::${AWS_ACCOUNT}:user/specific-iam-user"),
+            ),
+            description: "Role for authorized clients to access the API"
+        });
+
+        // Grant permission to invoke the API
+        clientRole.addToPolicy(
+            new PolicyStatement({
+                actions: ["execute-api:Invoke"],
+                resources: [api.arnForExecuteApi()]
+            })
+        );
+
+        // // Output the role ARN for reference
+        // new cdk.CfnOutput(this, "AuthorizedClientRoleArn", {
+        //     value: clientRole.roleArn,
+        //     description: "ARN of the IAM role for authorized clients"
+        // });
+
+        // // Output the API URL
+        // new cdk.CfnOutput(this, "ApiUrl", {
+        //     value: api.url,
+        //     description: "URL of the API Gateway"
+        // });
     }
 }
