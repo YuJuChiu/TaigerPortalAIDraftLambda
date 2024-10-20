d4f26f58f33a3564c3d8265fc57a8f3c2b7b794a
Leo <liyung.chen@tum.de>
2024-10-20 23:00:33 +0200

    change package name

--- test/tai_ger_portal_transcript_analyzer_lambda_cdk.test.ts
+++ test/tai_ger_portal_transcript_analyzer_lambda_cdk.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as TaiGerPortalTranscriptAnalyzerLambdaCdk from '../lib/tai_ger_portal_transcript_analyzer_lambda_cdk-stack';
+// import * as TaiGerPortalTranscriptAnalyzerLambdaCdk from '../lib/tai_ger_portal_lambda_cdk-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/tai_ger_portal_transcript_analyzer_lambda_cdk-stack.ts
+// example resource in lib/tai_ger_portal_lambda_cdk-stack.ts
 test("SQS Queue Created", () => {
     //   const app = new cdk.App();
     //     // WHEN
