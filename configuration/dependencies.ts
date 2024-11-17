85910ddf458045d94cd3937a83fde6a811f5a614
YuJuChiu <candychiu26@gmail.com>
2024-11-17 01:04:17 +0100

    fix aws account, github_token

--- configuration/dependencies.ts
+++ configuration/dependencies.ts
@@ -1,11 +1,11 @@
 // GitHub
 export const GITHUB_TOKEN =
-    "arn:aws:secretsmanager:us-east-1:669131042313:secret:beta/taigerportal-Hm1SLX";
-export const GITHUB_REPO = "TaiGerPortalLambda";
-export const GITHUB_OWNER = "LIYUNG";
+    "arn:aws:secretsmanager:us-east-1:322721758081:secret:beta/taigerportal-ajTWwF";
+export const GITHUB_REPO = "TaigerPortalAIDraftLambda";
+export const GITHUB_OWNER = "YuJuChiu";
 export const GITHUB_PACKAGE_BRANCH = "main";
 
-export const AWS_ACCOUNT = "669131042313";
+export const AWS_ACCOUNT = "503561438104";
 export const DOMAIN_NAME = "taigerconsultancy-portal.com";
 export const APP_NAME_TRANSCRIPT_ANALYZER = "TranscriptAnalyzer";
 export const TENANT_NAME = "TaiGer";
